d113314ae3b351322189a32ff9f80e4af8f3c60c
yachimoto <yachimoto0607@gmail.com>
2019-07-25 12:00:37 +0800

    project作成

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldFortitle: UITextField!
+    
+    
+    @IBOutlet weak var textFieldForContent: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func didClickButtom(_ sender: UIButton) {
+    }
+    
 
 }
 
